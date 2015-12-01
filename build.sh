cd584dddfbee19a74e1c4fd13015096dba905b20
Eggert Jóhannesson <eggert@hugsaser.is>
2015-12-01 14:13:43 +0000

    Add junit test reports to build process

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Require github credentials as environment variables
+: "${GIT_USER:?Need to set GIT_USER environment variable}"
+: "${GIT_PASS:?Need to set GIT_PASS environment variable}"
+
+# Make bash exit on any error, in piped commands or otherwise
+set -e
+set -o pipefail
+
+# Prepare tmp directory for building
+rm -rf /tmp/src
+mkdir -p /tmp/src
+cd /tmp/src
+
+# Get code and install dependencies
+git clone https://$GIT_USER:$GIT_PASS@github.com/eggertj12/hgop2015.git
+cd hgop2015
+npm install
+bower install
+
+# Build the masterpiece
+./dockerbuild.sh
