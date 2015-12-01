c06e4b5d4ac5251f92933375af1cc9c24507f58f
Eggert Jóhannesson <eggert@hugsaser.is>
2015-12-01 15:02:26 +0000

    Change build script to accomodate using git plugin in jenkins

--- build.sh
+++ build.sh
@@ -8,15 +8,16 @@
 set -e
 set -o pipefail
 
-# Prepare tmp directory for building
-cd ~
-rm -rf build/
-mkdir build/
-cd build/
+# # Prepare tmp directory for building
+# cd ~
+# rm -rf build/
+# mkdir build/
+# cd build/
+# 
+# # Get code and install dependencies
+# git clone https://$GIT_USER:$GIT_PASS@github.com/eggertj12/hgop2015.git
+# cd hgop2015
 
-# Get code and install dependencies
-git clone https://$GIT_USER:$GIT_PASS@github.com/eggertj12/hgop2015.git
-cd hgop2015
 npm install
 bower install
 
