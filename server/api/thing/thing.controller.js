1b52dea3d6b09b95d44ddce16328f4691a7fc90e
Eggert Jóhannesson <eggert@hugsaser.is>
2015-12-01 15:27:34 +0000

    Clean up build scripts and test automatic polling

--- server/api/thing/thing.controller.js
+++ server/api/thing/thing.controller.js
@@ -21,13 +21,13 @@ exports.index = function(req, res) {
   name : 'Server and Client integration',
   info : 'Built with a powerful and fun stack: MongoDB, Express, AngularJS, and Node.'
   }, {
-  name : 'Smart Building System',
+  name : 'Smart Build System',
   info : 'Build system ignores `spec` files, allowing you to keep tests alongside code. Automatic injection of scripts and styles into your index.html'
   },  {
   name : 'Modular Structure',
   info : 'Best practice client and server structures allow for more code reusability and maximum scalability'
   },  {
-  name : 'Optimized Build',
+  name : 'Optimizing Builds is cool',
   info : 'Build process packs up your templates as a single JavaScript payload, minifies your scripts/css/images, and rewrites asset names for caching.'
   },{
   name : 'Deployment Ready',
