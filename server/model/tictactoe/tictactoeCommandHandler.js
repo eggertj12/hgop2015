a36a5dca17c437111ea65e2cc1193cb28bfc1d4a
Eggert Jóhannesson <eggert@hugsaser.is>
2015-12-03 10:15:49 +0000

    Refactor tictactoe Command Handler to use a separate function/class for state

--- server/model/tictactoe/tictactoeCommandHandler.js
+++ server/model/tictactoe/tictactoeCommandHandler.js
@@ -1,8 +1,6 @@
-module.exports = function tictactoeCommandHandler(events) {
-	'use strict';
-	var gameCreatedEvent = events[0];
-	var gameJoinedEvent = events[1];
-	var gameState = {
+function tictactoeState() {
+
+	var state = {
 		board: [
 			['', '', ''],
 			['', '', ''],
@@ -11,71 +9,95 @@ module.exports = function tictactoeCommandHandler(events) {
 		nextPlayer: 'X',
 		numberOfMoves: 0
 	}
+
+	return {
+		/**
+		 * Build the board status from event collection
+		 */
+		'buildState': function (events) {
+			var counter = 0;
+			events.forEach(function (event) {
+				if (event.event === 'MovePlaced') {
+					state.board[event.boardY][event.boardX] = event.player;
+					counter++;
+					state.numberOfMoves = counter;
+				}
+			}, this);
+		},
+		
+		/**
+		 * Place a move on the board
+		 */
+		'placeMove': function (x, y, player) {
+			state.board[y][x] = player;
+			state.numberOfMoves++;
+		},
+		
+		/**
+		 * Check if given commmand is a valid move
+		 * returns event name on invalid move, undefined otherwise
+		 */
+		'checkMove': function(x, y, player) {
+			if (state.board[y][x] !== '') {
+				return 'AlreadyFilled';
+			}
+			if (player === 'X' && (state.numberOfMoves % 2) === 1) {
+				return 'InvalidPlayer';
+			}
+			if (player === 'O' && (state.numberOfMoves % 2) === 0) {
+				return 'InvalidPlayer';
+			}
+		},
+		
+		/**
+		 * Check if game is won
+		 * Returns the winning player if won, undefined otherwise
+		 */
+		'gameWon': function() {
+			// Check for winning column
+			for (var i = 0; i < 3; i++) {
+				var colFirst = state.board[0][i];
+				if (colFirst !== '' && (colFirst === state.board[1][i]) && (colFirst === state.board[2][i])) {
+					return colFirst;
+				}
+			}
 	
-	/**
-	 * Build the board status from event collection
-	 */
-	var buildBoard = function(events, state) {
-		var counter = 0;
-		events.forEach(function(event) {
-			if (event.event === 'MovePlaced') {
-				state.board[event.boardY][event.boardX] = event.player;
-				counter ++;
-				state.numberOfMoves = counter;
+			// Check for winning row
+			for (var i = 0; i < 3; i++) {
+				var rowFirst = state.board[i][0];
+				if (rowFirst !== '' && (rowFirst === state.board[i][1]) && (rowFirst === state.board[i][2])) {
+					return rowFirst;
+				}
 			}
-		}, this);	
-		return gameState;
-	};
-
-	/**
-	 * Check if given commmand is a valid move
-	 * returns event name on invalid move, undefined otherwise
-	 */
-	var checkMove = function(cmd, state) {
-		if (state.board[cmd.boardY][cmd.boardX] !== '') {
-			return 'AlreadyFilled';
-		}
-		if (cmd.player === 'X' && (state.numberOfMoves % 2) === 1) {
-			return 'InvalidPlayer';
-		}
-		if (cmd.player === 'O' && (state.numberOfMoves % 2) === 0) {
-			return 'InvalidPlayer';
-		}
-	};
 	
-	/**
-	 * Check if game is won
-	 */
-	var gameWon = function(cmd, state) {
-		// Check for winning column
-		for (var i = 0; i < 3; i++) {
-			var colFirst = state.board[0][i];
-			if (colFirst !== '' && (colFirst === state.board[1][i]) && (colFirst === state.board[2][i])) {
-				return colFirst;
+			// Check for winning diagonal 0,0 to 2,2
+			var diagFirst = state.board[0][0];
+			if (diagFirst !== '' && (diagFirst === state.board[1][1]) && (diagFirst === state.board[2][2])) {
+				return diagFirst;
 			}
-		}
-
-		// Check for winning row
-		for (var i = 0; i < 3; i++) {
-			var rowFirst = state.board[i][0];
-			if (rowFirst !== '' && (rowFirst === state.board[i][1]) && (rowFirst === state.board[i][2])) {
-				return rowFirst;
+	
+			// Check for winning diagonal 0,2 to 2,0
+			var diagFirst = state.board[0][2];
+			if (diagFirst !== '' && (diagFirst === state.board[1][1]) && (diagFirst === state.board[2][0])) {
+				return diagFirst;
 			}
+		},
+		/**
+		 * Check if game is a draw
+		 * Returns boolean draw or not
+		 */
+		'gameDrawn': function() {
+			return state.numberOfMoves === 9;
 		}
+	}
+}
 
-		// Check for winning diagonal 0,0 to 2,2
-		var diagFirst = state.board[0][0];
-		if (diagFirst !== '' && (diagFirst === state.board[1][1]) && (diagFirst === state.board[2][2])) {
-			return diagFirst;
-		}
-
-		// Check for winning diagonal 0,2 to 2,0
-		var diagFirst = state.board[0][2];
-		if (diagFirst !== '' && (diagFirst === state.board[1][1]) && (diagFirst === state.board[2][0])) {
-			return diagFirst;
-		}
-	};
-
+module.exports = function tictactoeCommandHandler(events) {
+	'use strict';
+	var gameCreatedEvent = events[0];
+	var gameJoinedEvent = events[1];
+	var state = tictactoeState();
+	
 	var handlers = {
 		'CreateGame': function (cmd) {
 			return [{
@@ -140,10 +162,10 @@ module.exports = function tictactoeCommandHandler(events) {
 			}
 			
 			// Game is valid, create state
-			gameState = buildBoard(events, gameState);
+			state.buildState(events);
 			
 			// Check for valid move
-			var result = checkMove(cmd, gameState); 
+			var result = state.checkMove(cmd.boardX, cmd.boardY, cmd.player);
 			if (result !== undefined) {
 				return [{
 					id: cmd.id,
@@ -155,9 +177,7 @@ module.exports = function tictactoeCommandHandler(events) {
 			}
 			
 			// Place move on board
-			gameState.board[cmd.boardY][cmd.boardX] = cmd.player;
-			gameState.numberOfMoves++;
-			
+			state.placeMove(cmd.boardX, cmd.boardY, cmd.player);
 			var placedEvent = {
 				id: cmd.id,
 				event: 'MovePlaced',
@@ -170,7 +190,7 @@ module.exports = function tictactoeCommandHandler(events) {
 			}; 
 
 			// Check for winning
-			result = gameWon(cmd, gameState);
+			result = state.gameWon();
 			if (result !== undefined) {
 				var wonEvent = {
 					id: cmd.id,
@@ -180,12 +200,12 @@ module.exports = function tictactoeCommandHandler(events) {
 					userName: cmd.userName,
 					timeStamp: cmd.timeStamp
 				};
-				
+
 				return [placedEvent, wonEvent];
 			}
 			
 			// Finally check for draw
-			if (gameState.numberOfMoves === 9) {
+			if (state.gameDrawn()) {
 				var drawnEvent = {
 					id: cmd.id,
 					event: 'GameDrawn',
@@ -193,7 +213,7 @@ module.exports = function tictactoeCommandHandler(events) {
 					userName: cmd.userName,
 					timeStamp: cmd.timeStamp
 				};
-				
+
 				return [placedEvent, drawnEvent];
 			}
 			
