e4f520bdc6957177668ffb1373fc3b0d73223319
Eggert Jóhannesson <eggert@hugsaser.is>
2015-12-07 23:52:07 +0000

    Implement fluid API acceptance testing. Also update Vagrantfile to prevent ssh conflict between boxes independent of starting order

--- server/acceptance/tictactoe.acceptance.js
+++ server/acceptance/tictactoe.acceptance.js
@@ -4,6 +4,78 @@ var should = require('should');
 var request = require('supertest');
 var acceptanceUrl = process.env.ACCEPTANCE_URL;
 
+// Set up fluid API for testing
+// Some test data
+const testCmdId = '23456';
+const testGameId = '98765';
+const testTimeStamp = Date.now();
+function user(userName) {
+  return {
+    'createsGame': function (gameName) {
+      var command = {
+        id: testCmdId,
+        gameId: testGameId,
+        comm: "CreateGame",
+        userName: userName,
+        name: gameName,
+        timeStamp: testTimeStamp
+      };
+
+      return command;
+    }
+  };
+}
+
+function given(command) {
+  const condition = {};
+
+  var givenAPI = {
+    'expect': (event) => {
+      condition.event = event;
+      return givenAPI;
+    },
+    'withName': (gameName) => {
+      condition.gameName = gameName;
+      return givenAPI;
+    },
+    'isOk': (done) => {
+      // Execute command
+      var req = request(acceptanceUrl);
+      req
+        .post('/api/createGame')
+        .type('json')
+        .send(command)
+        .end((err, res) => {
+          if (err) {
+            return done(err);
+          }
+
+          // Load the game history and verify that it fulfills the conditions
+          request(acceptanceUrl)
+            .get('/api/gameHistory/' + testGameId)
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .end(function (err, res) {
+              if (err) return done(err);
+              res.body.should.be.instanceof(Array);
+              should(res.body).eql(
+                [{
+                  "id": testCmdId,
+                  "gameId": testGameId,
+                  "event": condition.event,
+                  "userName": command.userName,
+                  "name": condition.gameName,
+                  "timeStamp": testTimeStamp
+                }]);
+              done();
+            });
+
+        });
+    }
+  };
+
+  return givenAPI;
+}
 
 describe('TEST ENV GET /api/gameHistory', function () {
 
@@ -13,9 +85,9 @@ describe('TEST ENV GET /api/gameHistory', function () {
 
   it('should execute same test using old style', function (done) {
 
-    var command =     {
-      id : "1234",
-      gameId : "999",
+    var command = {
+      id: "1234",
+      gameId: "999",
       comm: "CreateGame",
       userName: "Gulli",
       name: "TheFirstGame",
@@ -51,12 +123,9 @@ describe('TEST ENV GET /api/gameHistory', function () {
   });
 
 
-   it('Should execute fluid API test', function (done) {
-     /*
-     given(user("YourUser").createsGame("TheFirstGame"))
-     .expect("GameCreated").withName("TheFirstGame").isOk(done);
-      */
-     done();
-   });
+  it('Should execute fluid API test', function (done) {
+    given(user("YourUser").createsGame("TheFirstGame"))
+    .expect("GameCreated").withName("TheFirstGame").isOk(done);
+  });
 
 });
